94e13327a3c4cce52134d562b2d2c0e8e82e7484
apavlyuc <apavlyuc@gmail.com>
2019-03-08 14:05:01 +0200

    rm git log

--- main.cc
+++ main.cc
@@ -12,18 +12,6 @@ using std::vector;
 
 int			main(int args_count, char **args)
 {
-<<<<<<< HEAD
-	if (ac != 3)
-		return (0);
-
-	int val1 = std::stoi(av[1]);
-	int val2 = std::stoi(av[2]);
-
-	BigNumber num1(val1), num2(val2);
-
-	num1.print_info();
-	num2.print_info();
-=======
 	if (3 != args_count) {
 		std::cout << "usage: " << args[0] << " arg1 arg2" << std::endl;
 		return 1;
@@ -38,6 +26,5 @@ int			main(int args_count, char **args)
 	BigNumber result = lhs - rhs;
 	std::cout << "result: "; result.print_info();
 	
->>>>>>> f34eb22493603eaf8af4a74223a2a134a59be82f
 	return (0);
 }
